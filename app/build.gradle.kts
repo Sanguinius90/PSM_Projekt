0631b1991738b6ca58c41caef9211373d4a8103d
Sanguinius <kacperkotnisz@gmail.com>
2025-04-18 22:56:21 +0200

    Backu przed dodanie bazy danych room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,9 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
